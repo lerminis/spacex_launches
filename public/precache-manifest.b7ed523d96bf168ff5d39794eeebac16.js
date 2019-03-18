62a47630d0d7d33e80124d9c33290303247f951d
Stephen <stephen.lerminiaux@gmail.com>
2019-03-17 22:54:34 -0600

    Built public folder

---
+++ public/precache-manifest.b7ed523d96bf168ff5d39794eeebac16.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "6662c20158325c7d5dbe",
+    "url": "/static/js/main.429a20ce.chunk.js"
+  },
+  {
+    "revision": "2aa4ef737732ac36db24",
+    "url": "/static/js/2.f9daf2f9.chunk.js"
+  },
+  {
+    "revision": "6662c20158325c7d5dbe",
+    "url": "/static/css/main.93cda1e9.chunk.css"
+  },
+  {
+    "revision": "6a0567f86e858c7a9ba08ee864b3855a",
+    "url": "/index.html"
+  }
+];
